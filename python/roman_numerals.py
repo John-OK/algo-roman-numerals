91ce08615c65c553f6485f6e4d01151caea40f92
JohnOK <john.ok.esp@gmail.com>
2022-05-28 19:55:09 +0200

    started python version

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,34 @@
+# 1. Write a method TO_ROMAN, TO_ROMAN takes in INPUT_NUMBER (an arabic number)
+# 2. Create a OUTPUT string, set to ''
+# 3. Create a ROMAN_NUMERAL_TO_ARABIC_MAP that includes roman numerals as keys, 
+#    arabic numbers as values
+# 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and 
+#    ARABIC_NUMBER
+# 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
+# 6. If EVENLY_DIVISIBLE_TIMES >= 1
+# 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
+# 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
+# 7. Return OUTPUT
+
 def to_roman(num):
-    # write your code here!
+    converted_to_roman = ""
+    map_roman_to_number = {
+        'I' : 1,
+        'IV': 4,
+        'V' : 5,
+        'IX': 9,
+        'X' : 10,
+        'XL': 40,
+        'L' : 50,
+        'XC': 90,
+        'C' : 100,
+        'CD': 400,
+        'D' : 500,
+        'CM': 900,
+        'M' : 1000,
+    }
+    order_of_conversion = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
+
+What was the last feedback you received from an employee and how was it implemented by the company and what was its impact (or thereabouts...)
+
